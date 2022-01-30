40ce47948527730bf5a028fcf3056ff2b30752fd
koki.hatano <koki.hatano@zozo.com>
2022-01-31 08:56:26 +0900

    Add Access-Control-Allow-Origin

--- backend/app/adapter/http/middleware/cors_middleware.go
+++ backend/app/adapter/http/middleware/cors_middleware.go
@@ -8,7 +8,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, X-CSRF-Token")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000, https://toebeans.tk")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
