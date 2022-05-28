e2049e84ea215ba9f46886e7456247ff35bbf711
gold-kou <gold.kou55@gmail.com>
2022-05-28 15:50:51 +0900

    ECS NATGateway ON

--- infra/environments/prd/vpc.tf
+++ infra/environments/prd/vpc.tf
@@ -85,55 +85,55 @@ resource "aws_subnet" "private_1" {
 }
 
 # MEMO nat gatewayはお金かかるので開発中は一時的に消す
-# resource "aws_route_table" "private_0" {
-#   vpc_id = aws_vpc.toebeans.id
-# }
+resource "aws_route_table" "private_0" {
+  vpc_id = aws_vpc.toebeans.id
+}
 
-# resource "aws_route_table" "private_1" {
-#   vpc_id = aws_vpc.toebeans.id
-# }
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.toebeans.id
+}
 
-# resource "aws_route_table_association" "private_0" {
-#   subnet_id      = aws_subnet.private_0.id
-#   route_table_id = aws_route_table.private_0.id
-# }
+resource "aws_route_table_association" "private_0" {
+  subnet_id      = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
 
-# resource "aws_route_table_association" "private_1" {
-#   subnet_id      = aws_subnet.private_1.id
-#   route_table_id = aws_route_table.private_1.id
-# }
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
 
-# resource "aws_route" "private_0" {
-#   route_table_id         = aws_route_table.private_0.id
-#   nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
-#   destination_cidr_block = "0.0.0.0/0"
-# }
+resource "aws_route" "private_0" {
+  route_table_id         = aws_route_table.private_0.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
 
-# resource "aws_route" "private_1" {
-#   route_table_id         = aws_route_table.private_1.id
-#   nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
-#   destination_cidr_block = "0.0.0.0/0"
-# }
+resource "aws_route" "private_1" {
+  route_table_id         = aws_route_table.private_1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
 
-# resource "aws_eip" "nat_gateway_0" {
-#   vpc        = true
-#   depends_on = [aws_internet_gateway.toebeans]
-# }
+resource "aws_eip" "nat_gateway_0" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.toebeans]
+}
 
-# resource "aws_eip" "nat_gateway_1" {
-#   vpc        = true
-#   depends_on = [aws_internet_gateway.toebeans]
-# }
+resource "aws_eip" "nat_gateway_1" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.toebeans]
+}
 
-# resource "aws_nat_gateway" "nat_gateway_0" {
-#   allocation_id = aws_eip.nat_gateway_0.id
-#   subnet_id     = aws_subnet.public_0.id
-#   depends_on    = [aws_internet_gateway.toebeans]
-# }
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id     = aws_subnet.public_0.id
+  depends_on    = [aws_internet_gateway.toebeans]
+}
 
-# resource "aws_nat_gateway" "nat_gateway_1" {
-#   allocation_id = aws_eip.nat_gateway_1.id
-#   subnet_id     = aws_subnet.public_1.id
-#   depends_on    = [aws_internet_gateway.toebeans]
-# }
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = [aws_internet_gateway.toebeans]
+}
 # ここまで
